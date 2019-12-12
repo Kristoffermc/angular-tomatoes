55419287968c698ad37daf631f5c8f8384bd1505
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2019-12-12 11:49:41 +0100

    Init?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RestService} from './services/rest.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tomatoes';
+
+  constructor(private rest: RestService) {
+    this.rest.changeLight(-1).toPromise().then(res => {
+      console.log(res);
+    });
+
+    this.rest.getSensorDataByType('temperature', 20).toPromise().then(res => {
+      console.log(res);
+    });
+  }
 }
---
+++ src/app/services/rest.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+
+  getSensorDataByType(sensortype, count) {
+    return this.httpClient.get('https://piebrain.net/api/sensorData/name/' + sensortype, {
+      headers: {
+        limit: count
+      }
+    });
+  }
+
+
+  changeLight(value) {
+    return this.httpClient.post('https://piebrain.net/api/leds', {
+        ledId: '1',
+        ledValue: value
+      }
+    );
+  }
+}
